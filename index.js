9c10e1d517ba9008bded92107d8d4c83600a75e4
Chu-rill <churchillrimamskep@gmail.com>
2024-09-05 03:11:25 +0100

    done with e url shortener

--- index.js
+++ index.js
@@ -5,14 +5,10 @@ const mongoose = require("mongoose");
 const ShortUrl = require("./model/shortUrl");
 const app = express();
 app.use(cors());
+app.use(express.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
-app.get("/", (req, res) => {
-  res.render("index");
-});
-app.post("/shortUrls", (req, res) => {});
 const port = process.env.PORT;
 const URI = process.env.MONGODB_URI || "mongodb://localhost:27017/urlShortener";
-
 mongoose
   .connect(URI, {
     // serverSelectionTimeoutMS: 3000000,
@@ -28,6 +24,18 @@ mongoose
     console.error("Connection failed", error);
   });
 
-// app.listen(port, () => {
-//   console.log(`server runing on port ${port}`);
-// });
+app.get("/", async (req, res) => {
+  const shortUrls = await ShortUrl.find();
+  res.render("index", { shortUrls: shortUrls });
+});
+app.post("/shortUrls", async (req, res) => {
+  await ShortUrl.create({ full: req.body.fullUrl });
+  res.redirect("/");
+});
+app.get("/:shortUrl", async (req, res) => {
+  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
+  if (shortUrl == null) return res.sendStatus(404);
+  shortUrl.clicks++;
+  shortUrl.save();
+  res.redirect(shortUrl.full);
+});
